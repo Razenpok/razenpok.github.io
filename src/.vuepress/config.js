6c882f7eca16ce9b7323700bf9292f380acdbca2
Andrei Andreev <Razenpok@outlook.com>
2021-07-09 15:27:47 +0300

    Add gtag

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -14,5 +14,21 @@ module.exports = {
     '@vuepress/google-analytics': {
       'ga': 'UA-89958529-1'
     }
-  }
+  },
+  head: [
+    [
+        'script',
+        {
+            async: true,
+            src: 'https://www.googletagmanager.com/gtag/js?id=G-MZH7HXNHT8',
+        },
+    ],
+    [
+        'script',
+        {},
+        [
+            "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-MZH7HXNHT8');",
+        ],
+    ],
+  ],
 }
